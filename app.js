266e6f1e7199ce0021935bf4f3bf6c6a37b84823
Sourav <sourav.singh@emoha.com>
2022-09-24 13:15:50 +0530

    fix: added stages zzd

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const puppeteer = require("puppeteer-core");
-// const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
+// const chromium = require("chromium");
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", async (req, res) => {
-  const browser = await puppeteer.launch(
-    "--no-sandbox",
-    "--disable-web-security"
-  );
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
   const page = await browser.newPage();
 
   await page.goto("https://google.com");
