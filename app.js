803446fa079bab1af0a3a72e32225c6be377253b
Sourav <sourav.singh@emoha.com>
2022-09-24 18:04:12 +0530

    fix:funn

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
 const express = require("express");
-// const puppeteer = require("puppeteer-core");
 const puppeteer = require("puppeteer");
 const app = express();
 const port = process.env.PORT || 3001;
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const getDataFromXPath = async (xPath, page) => {
+  return await page.$x(xPath);
+};
+
 app.get("/", async (req, res) => {
-  const browser = await puppeteer.launch(
-    "--no-sandbox",
-    "--disable-web-security"
-  );
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto("https://google.com");
+  await page.goto("https://app.hrone.cloud/app");
+  await sleep(1000);
   const title = await page.title();
-  setTimeout(function () {
-    console.log("NEwwww");
-  }, 20000);
+  setTimeout(async function () {
+    console.log("chala");
+    await page.waitForSelector("#hrone-username");
+    await page.type("#hrone-username", "sourav.singh@emoha.com");
+    await getDataFromXPath(
+      "/html/body/app-root/app-login/div/div/div/div[2]/div/div/app-login-page/div/div/div[1]/div[2]/button",
+      page
+    )
+      .then((e) => e[0].click())
+      .catch((error) => console.log(error));
+    await sleep(1000);
+    await page.type("#hrone-password", "Sign123@##");
+    await getDataFromXPath(
+      "/html/body/app-root/app-login/div/div/div/div[2]/div/div/app-login-page/div/div/div[3]/div[3]/button[2]",
+      page
+    )
+      .then((e) => e[0].click())
+      .catch((error) => console.log(error));
+
+    // await getDataFromXPath(
+    //   "/html/body/app-root/app-login/div/div/div/div[2]/div/div/app-login-page/div/div/div[3]/div[3]/button[2]",
+    //   page
+    // ).click();
+    // await browser.close();
+  }, 1000);
   console.log("Page Title : " + title);
   res.send(`hello + ${title}`);
 });
