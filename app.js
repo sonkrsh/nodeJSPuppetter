73312d885fe22ecaa6d44524bd72b45e3ef61390
Sourav <sourav.singh@emoha.com>
2022-09-24 13:07:46 +0530

    fix: added stages zz

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const puppeteer = require("puppeteer-core");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
+// const puppeteer = require("puppeteer");
 const app = express();
 const port = process.env.PORT || 3001;
 
