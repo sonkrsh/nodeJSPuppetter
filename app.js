0e997b032bc955925d6c8c8bd6164a2f18bf39fa
Sourav <sourav.singh@emoha.com>
2022-09-24 13:27:49 +0530

    fix: added

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const chromium = require("chrome-aws-lambda");
-// const chromium = require("chromium");
+// const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer");
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", async (req, res) => {
-  const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
+  const browser = await puppeteer.launch(
+    "--no-sandbox",
+    "--disable-web-security"
+  );
   const page = await browser.newPage();
 
   await page.goto("https://google.com");
