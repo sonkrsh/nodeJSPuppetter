c702bd260340e5b8e09e42bbdf2faa03c6785e7f
Sourav <sourav.singh@emoha.com>
2022-09-26 20:50:36 +0530

    fix: added in lead owneerer

--- app.js
+++ app.js
@@ -12,38 +12,42 @@ const getDataFromXPath = async (xPath, page) => {
 };
 
 app.get("/", async (req, res) => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
 
   await page.goto("https://app.hrone.cloud/app");
   await sleep(1000);
   const title = await page.title();
-  setTimeout(async function () {
-    console.log("chala");
-    await page.waitForSelector("#hrone-username");
-    await page.type("#hrone-username", "sourav.singh@emoha.com");
-    await getDataFromXPath(
-      "/html/body/app-root/app-login/div/div/div/div[2]/div/div/app-login-page/div/div/div[1]/div[2]/button",
-      page
-    )
-      .then((e) => e[0].click())
-      .catch((error) => console.log(error));
-    await sleep(1000);
-    await page.type("#hrone-password", "Sign123@##");
-    await getDataFromXPath(
-      "/html/body/app-root/app-login/div/div/div/div[2]/div/div/app-login-page/div/div/div[3]/div[3]/button[2]",
-      page
-    )
-      .then((e) => e[0].click())
-      .catch((error) => console.log(error));
+  // setTimeout(async function () {
+  //   console.log("chala");
+  //   await page.waitForSelector("#hrone-username");
+  //   await page.type("#hrone-username", "sourav.singh@emoha.com");
+  //   await getDataFromXPath(
+  //     "/html/body/app-root/app-login/div/div/div/div[2]/div/div/app-login-page/div/div/div[1]/div[2]/button",
+  //     page
+  //   )
+  //     .then((e) => e[0].click())
+  //     .catch((error) => console.log(error));
+  //   await sleep(1000);
+  //   await page.type("#hrone-password", "Sign123@##");
+  //   await getDataFromXPath(
+  //     "/html/body/app-root/app-login/div/div/div/div[2]/div/div/app-login-page/div/div/div[3]/div[3]/button[2]",
+  //     page
+  //   )
+  //     .then((e) => e[0].click())
+  //     .catch((error) => console.log(error));
 
-    // await getDataFromXPath(
-    //   "/html/body/app-root/app-login/div/div/div/div[2]/div/div/app-login-page/div/div/div[3]/div[3]/button[2]",
-    //   page
-    // ).click();
-    // await browser.close();
-  }, 1000);
+  //   // await getDataFromXPath(
+  //   //   "/html/body/app-root/app-login/div/div/div/div[2]/div/div/app-login-page/div/div/div[3]/div[3]/button[2]",
+  //   //   page
+  //   // ).click();
+  //   // await browser.close();
+  // }, 1000);
   console.log("Page Title : " + title);
+  await browser.close();
   res.send(`hello + ${title}`);
 });
 
